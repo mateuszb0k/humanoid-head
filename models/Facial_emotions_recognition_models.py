e37e40bd5da26518c3100e2537063739015f3edf
Baranittto <mikolaj19032003@gmail.com>
2026-04-03 23:22:59 +0200

    Hog_for_RPI

--- models/Facial_emotions_recognition_models.py
+++ models/Facial_emotions_recognition_models.py
@@ -10,7 +10,7 @@ from tensorflow.keras.regularizers import l2
 from sklearn.utils.class_weight import compute_class_weight
 from tensorboard.plugins.hparams import api as hp
 from pathlib import Path
-from tfboard import get_confusion_matrix, plot_confusion_matrix
+from models.tfboard import get_confusion_matrix, plot_confusion_matrix
 import json
 
 # disable logging output from TensorFLow
